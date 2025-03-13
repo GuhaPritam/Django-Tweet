22f6d6e434e5f7302200eb908d927f87a4a23a65
GuhaPritam <pguha3828@gmail.com>
2025-03-13 16:07:32 +0530

    created tweet_list in view

--- tweet/tweet_app/views.py
+++ tweet/tweet_app/views.py
@@ -1,5 +1,12 @@
 from os import name
 from django.shortcuts import render
+from .models import Tweet
+from .forms import TweetForm
+from django.shortcuts import get_list_or_404
 
 def index(request):
     return render(request, 'index.html')
+
+def tweet_list(request):
+    tweets = Tweet.objects.all().order_by('-created_at')
+    return render(request, 'tweet_list.html', {'tweets': tweets})
