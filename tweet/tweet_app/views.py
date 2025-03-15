b97d766fc3c71ca98ff5119ea9f8b2915997a2d8
GuhaPritam <pguha3828@gmail.com>
2025-03-15 18:36:39 +0530

    added edit tweet function

--- tweet/tweet_app/views.py
+++ tweet/tweet_app/views.py
@@ -1,15 +1,16 @@
 from os import name
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render,redirect
 from .models import Tweet
 from .forms import TweetForm
-from django.shortcuts import get_list_or_404, redirect
 
 def index(request):
     return render(request, 'index.html')
 
+
 def tweet_list(request):
     tweets = Tweet.objects.all().order_by('-created_at')
     return render(request, 'tweet_list.html', {'tweets': tweets})
+
 
 def tweet_create(request):
     if request.method == "POST":
@@ -21,4 +22,18 @@ def tweet_create(request):
             return redirect('tweet_list')
     else:
         form = TweetForm()
+    return render(request, 'tweet_form.html', {'form': form})
+
+
+def edit_tweet(request, tweet_id):
+    tweet = get_object_or_404(Tweet, pk=tweet_id, user=request.user)
+    if request.method == "POST":
+        form = TweetForm(request.POST, request.FILES)
+        if form.is_valid():
+            tweet = form.save(commit=False)
+            tweet.user = request.user
+            tweet.save()
+            return redirect('tweet_list')
+    else:
+        form = TweetForm(instance=tweet)
     return render(request, 'tweet_form.html', {'form': form})
